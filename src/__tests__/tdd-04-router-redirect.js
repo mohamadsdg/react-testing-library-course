328624a1f115ba6b3872eb4ba19a8631662f7fcd
mohamadsdg <mohamad.r.sadeghi93@gmail.com>
2020-06-04 01:33:40 +0430

    tdd react-router

---
+++ src/__tests__/tdd-04-router-redirect.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {render, fireEvent, wait} from '@testing-library/react'
+import {Redirect as MockRedirect} from 'react-router'
+import {savePost as mockSavePost} from '../api'
+import {Editor} from '../post-editor-04-router-redirect'
+
+jest.mock('react-router', () => {
+  return {
+    Redirect: jest.fn(() => null),
+  }
+})
+jest.mock('../api')
+
+test('render a form with title,content,submit,tags', async () => {
+  const fakeUser = {id: 'user-1'}
+  const {getByText, getByLabelText} = render(<Editor user={fakeUser} />)
+  const fakePost = {
+    title: 'Test Title',
+    content: 'Test content',
+    tags: ['tag1', 'tag2'],
+  }
+  getByLabelText(/title/i).value = fakePost.title
+  getByLabelText(/content/i).value = fakePost.content
+  getByLabelText(/tags/i).value = fakePost.tags.join(',')
+  const submit = getByText(/submit/i)
+
+  fireEvent.click(submit)
+  expect(submit).toBeDisabled()
+
+  expect(mockSavePost).toHaveBeenCalledWith({
+    ...fakePost,
+    authorId: fakeUser.id,
+  })
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
+
+  await wait(() => {
+    expect(MockRedirect).toHaveBeenCalledTimes(1)
+    expect(MockRedirect).toHaveBeenCalledWith({to: '/'}, {})
+  })
+})
